96b3388bd490bdce8be146f162e7b68937105726
zBlatzX <fabianorellana67@gmail.com>
2024-11-18 02:11:26 -0300

    feat: home y cambios en navbar

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { CriminalListComponent } from './pages/criminal-list/criminal-list.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full',},
+    { path: 'home', component:HomeComponent},
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: 'criminals', component: CriminalListComponent, },
---
+++ frontend/src/app/pages/home/home.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [NavbarComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  crimes = [
+    {
+      title: 'Robo de Grandes Dimensiones',
+      description: 'Un caso resuelto donde se recuperaron millones de dólares robados en una operación criminal internacional.',
+      image: 'https://via.placeholder.com/500x300?text=Robo',
+      imagePosition: 'left' // 'left' o 'right' para alternar la posición
+    },
+    {
+      title: 'Asalto a Mano Armada',
+      description: 'Rápida acción de la policía resolvió el asalto a mano armada que aterrorizó a los ciudadanos en el centro de la ciudad.',
+      image: 'https://via.placeholder.com/500x300?text=Asalto',
+      imagePosition: 'right'
+    },
+    {
+      title: 'Fraude Bancario Internacional',
+      description: 'Se logró desmantelar una red de fraude bancario internacional que afectó a miles de personas.',
+      image: 'https://via.placeholder.com/500x300?text=Fraude',
+      imagePosition: 'left'
+    }
+  ];
+
+  constructor(private router: Router) {}
+
+  goToCriminals() {
+    this.router.navigate(['/criminals']);
+  }
+}
